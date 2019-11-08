42e4459b5c1af128be8b85335fb2f8a66df92ad3
Denis Arkhipov <imbot@inbox.ru>
2019-11-08 19:19:20 +0300

    feat: add pencil

--- palette/index.js
+++ palette/index.js
@@ -2,7 +2,7 @@ const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
 const buttons = document.querySelectorAll('.options--list-item:not(.inactive):not(.colors)');
 
-function draw(arr) {
+function drawFromArr(arr) {
     if (canvas && canvas.getContext) {
         arr.forEach((row, rowIdx) => {
             row.forEach((column, colIdx) => {
@@ -17,7 +17,7 @@ function draw(arr) {
     } else throw new Error('Canvas Error');
 }
 
-function prefillCanvas() {
+/*function prefillCanvas() {
     let xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/stage-2/codejam-canvas/data/4x4.json');
     xhr.responseType = 'json';
@@ -25,16 +25,36 @@ function prefillCanvas() {
     xhr.onload = () => {
         let arr = [];
         arr = xhr.response;
-        draw(arr);
+        drawFromArr(arr);
     }
     xhr.onerror = () => {
         throw new Error('Data Error');
     }
 }
 
-prefillCanvas();
+prefillCanvas();*/
 
-canvas.addEventListener('click', () => {
+function draw(x, y) {
+    let startX = 0;
+    let startY = 0;
+    for(; startX <= 512, startY <= 512; startX += 128, startY += 128) {
+        if((startX - x >= -128) && (startY - y >= -128)) break;
+        if(startX - x >= -128) startX -= 128;
+        if(startY - y >= -128) startY -= 128;
+    }
+    console.log(startX, startY);
+    let finX = startX + 128;
+    let finY = startY + 128;
+    console.log(finX, finY);
+    if(canvas && canvas.getContext) {
+        ctx.fillStyle = '#00ff00';
+        ctx.fillRect(startX, startY, finX, finY);
+        console.log(startX, startY);
+        console.log(finX, finY);
+    } else throw new Error('Canvas Error');
+}
+
+canvas.addEventListener('mouseup', (e) => {
     let activeButtonId;
     buttons.forEach((button) => {
         if(button.classList.contains('active')) activeButtonId = button.getAttribute('id');
@@ -44,7 +64,18 @@ canvas.addEventListener('click', () => {
     if(activeButtonId === 'bucket') {
         drawBucket('000000');
     }
+
+    if(activeButtonId === 'pencil') {
+        pencilDraw(e);
+    }
 })
+
+function pencilDraw(e) {
+    let x = e.clientX - canvas.offsetLeft;
+    let y = e.clientY - canvas.offsetTop;
+    console.log(x, y);
+    draw(x, y);
+}
 
 function drawBucket(color) {
     if (canvas && canvas.getContext) {
